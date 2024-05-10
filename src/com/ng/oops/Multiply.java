f900bc32f7221e2485735a8ddec2f228f0f04d3a
aryannitin giri <nehagiri9146@gmail.com>
2024-05-10 16:02:58 +0530

    example in polymorphism

---
+++ src/com/ng/oops/Multiply.java
@@ -0,0 +1,11 @@
+package com.ng.oops;
+
+public class Multiply {
+	
+	public int multiply(int a , int b) {
+		return a*b;
+	}
+    public int multiply(int a ,int b,int c) {
+    	return a*b*c;
+    }
+}
---
+++ src/com/ng/oops/MultiplyClient.java
@@ -0,0 +1,15 @@
+package com.ng.oops;
+
+public class MultiplyClient {
+
+	public static void main(String[] args) {
+		
+		Multiply m1 = new Multiply();
+		
+		System.out.println(m1.multiply(2, 3));
+		
+		System.out.println(m1.multiply(10, 5, 2));
+
+	}
+
+}
